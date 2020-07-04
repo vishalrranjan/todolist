d1b7ca5cb26478792d87f52f72174727f5a817e5
vishalrranjan <vishalkr275@gmail.com>
2020-07-04 23:51:17 +0530

    Updates

---
+++ precache-manifest.16a6d305484472c687c07583afc593a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97a8c20ae8bba69bc7c01f5cf6452e6",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "8d2f0b265a7cacc69f1e",
+    "url": "/todolist/static/css/main.ffdef5f1.chunk.css"
+  },
+  {
+    "revision": "85b03b4a2be1385e14de",
+    "url": "/todolist/static/js/2.dd255c11.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todolist/static/js/2.dd255c11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d2f0b265a7cacc69f1e",
+    "url": "/todolist/static/js/main.4b019cf5.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
